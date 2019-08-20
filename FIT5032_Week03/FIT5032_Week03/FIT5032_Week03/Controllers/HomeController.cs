7b75d85457ed86fb79c716a77b36876deb8498c6
zshe0005 <zshe0005@student.monash.edu>
2019-08-21 02:05:59 +1000

    FIT5032_W03

---
+++ FIT5032_Week03/FIT5032_Week03/FIT5032_Week03/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_Week03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+            HelloWorld.Hello hello = new HelloWorld.Hello();
+
+            ViewBag.Message = hello.GetHello();
+
+            Exercise.ExampleDictionary ed = new Exercise.ExampleDictionary();
+
+            ed.Example();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
